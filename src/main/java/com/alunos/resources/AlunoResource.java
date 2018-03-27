b632da97db14eba5d3386e9f4d06c8f71993fbef
Eneylton <eneylton@hotmail.com>
2018-03-27 12:45:12 -0300

    Crud Aluno - Completo

--- src/main/java/com/alunos/resources/AlunoResource.java
+++ src/main/java/com/alunos/resources/AlunoResource.java
@@ -1,5 +1,6 @@
 package com.alunos.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alunos.dto.AlunoDTO;
 import com.alunos.model.Aluno;
@@ -47,6 +50,27 @@ public class AlunoResource {
 		Page<AlunoDTO> listDto = list.map(obj -> new AlunoDTO(obj));  
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody AlunoDTO objDto) {
+        Aluno obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody AlunoDTO objDto, @PathVariable Long id) {
+		Aluno obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
